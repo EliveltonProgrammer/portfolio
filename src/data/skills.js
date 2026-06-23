df8118111ae613e60d1ef74850df5a591881072a
Elivelton Almeida <eliveltoncarriel.almeida@hotmail.com>
2026-06-23 15:06:34 -0300

    niveis proficiencia

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,14 +3,14 @@ export const skills = [
         id: "backend",
         icon: "FaCode",
         title: "Backend",
-        items: ".NET • ASP.NET Core • MVC • Web API • C# • C++ • EF Core • LINQ",
+        items: ".NET 4.8 • ASP.NET Core 8.0 • MVC • Web API • C# • C++ • EF Core • LINQ",
         level: "50%"
     },
     {
         id: "frontend",
         icon: "FaLaptopCode",
         title: "Frontend",
-        items: "Windows Forms • Razor Pages • Blazor • HTML • CSS • JavaScript • TypeScript • React",
+        items: "Windows Forms 4.8 • .NET MAUI • Razor Pages • React • JavaScript • HTML • CSS",
         level: "60%"
     },
     {
@@ -18,13 +18,34 @@ export const skills = [
         icon: "FaDatabase",
         title: "Banco de Dados",
         items: "SQL Server • MySQL • PostgreSQL • SQLite",
-        level: "90%"
+        level: "70%"
     },
     {
-        id: "tools",
+        id: "tools-Dev",
         icon: "FaTools",
-        title: "Ferramentas",
-        items: "Visual Studio • VS Code • Git • GitHub • JIRA • Kanban • Movidesk",
+        title: "IDEs",
+        items: "Microsoft Visual Studio • VS Code • Android Studio • Arduino IDE",
+        level: "70%"
+    },
+    {
+        id: "tools-Dev2",
+        icon: "FaTools",
+        title: "Microsoft Power Plataform",
+        items: "Power Apps • Power Automate",
+        level: "20%"
+    },
+    {
+        id: "tools-Dev3",
+        icon: "FaTools",
+        title: "Versionamento",
+        items: "Git • GitHub",
+        level: "50%"
+    },
+    {
+        id: "tools-Dev4",
+        icon: "FaTools",
+        title: "Ferramentas de Gestão e Modelagem",
+        items: "Astah UML • JIRA • Kanban • Movidesk • Servidesk",
         level: "70%"
     },
     {
@@ -32,7 +53,7 @@ export const skills = [
         icon: "FaServer",
         title: "Infraestrutura",
         items: "IIS • Deploy • Hospedagem • Configuração de ambientes",
-        level: "100%"
+        level: "90%"
     },
     {
         id: "support",
